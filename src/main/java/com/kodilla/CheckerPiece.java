1ca13e104e6eeee5a73aa5df2810c700923d8de6
Oskar Anczyk <myamy45.5@gmail.com>
2021-01-11 09:32:18 +0100

    Made basic CheckerPiece class and enum for piece color

--- src/main/java/com/kodilla/CheckerPiece.java
+++ src/main/java/com/kodilla/CheckerPiece.java
@@ -4,9 +4,26 @@ import javafx.scene.image.Image;
 
 public class CheckerPiece {
     private Image image;
+    private PieceColor color;
 
-    public CheckerPiece(Image image){
+    public CheckerPiece(Image image,PieceColor color){
+        this.image = image;
+        this.color = color;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
         this.image = image;
     }
 
+    public PieceColor getColor() {
+        return color;
+    }
+
+    public void setColor(PieceColor color) {
+        this.color = color;
+    }
 }
---
+++ src/main/java/com/kodilla/PieceColor.java
@@ -0,0 +1,6 @@
+package com.kodilla;
+
+public enum PieceColor {
+    WHITE,
+    BLACK
+}
